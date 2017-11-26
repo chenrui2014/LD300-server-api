5f8305930cee01b03920ece3b9d78b9323f3c925
ninao99 <ninao99@foxmail.com>
2017-11-26 17:47:12 +0800

    重新文件引用

--- src/test/startup/server/ipc_server_child.test.js
+++ src/test/startup/server/ipc_server_child.test.js
@@ -1,4 +1,4 @@
-const Server=require('../../../app/servers/ipc_server_child');
+const Server=require('../../../servers/ipc_server_child');
 
 describe('ipc_server_child服务测试',()=>{
     it('启动',(done)=>{
