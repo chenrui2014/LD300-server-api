8fe3bf787fd099b87ff0f0ebdd5674db15161af0
ninao99 <ninao99@foxmail.com>
2017-11-26 17:45:52 +0800

    http-proxy locahost修改为127.0.01，解决转发问题

--- src/servers/ipc_server_child.js
+++ src/servers/ipc_server_child.js
@@ -2,10 +2,9 @@
  * Created by Luky on 2017/9/4.
  */
 
-require('babel-polyfill');
-// import Logger from '../logger';
-// import connect from '../db';
-
+//import 'babel-polyfill';
+//import connect from '../db';
+const connect =require('../db/index');
 const http = require('http');
 const _=require('lodash');
 const Live=require('./ipc_live_server');
@@ -17,7 +16,6 @@ const ptzLock=_.get(config,'ipc.ptzLock',15000);
 const url=require('url');
 const logger={};
 const {Parser}=require('../log/log');
-const qs = require("querystring");
 let port=0;
 
 let send=function (data){
@@ -40,25 +38,15 @@ const ptzFun=[
 
 let lives={length:0};
 
-// (async() => {try {
-//     const connection = await connect();
-//     Logger.info('connected to MongoDB %s:%s/%s', connection.host, connection.port, connection.name);
-// }catch (error) {
-//     Logger.error(error);
-//     process.exit(-1);
-// }})();
-
-
-
-async function getLive(ipcData) {
-    let l=lives[ipcData.id];
+async function getLive(id) {
+    let l=lives[id];
     if(l) return l;
-    let ipc=await IPCFactory.getIPC(ipcData).catch( async ()=>{
-        logger.warn('请求的摄像头不存在',{id:ipcData.id});
+    let ipc=await IPCFactory.getIPC(id).catch( async ()=>{
+        logger.warn('请求的摄像头不存在',{id:id});
         await Promise.resolve(null);
     });
     if(!ipc)return null;
-    return lives[ipcData.id]=new Live(server,ipc,'',{autoClose:true});
+    return lives[id]=new Live(server,ipc,'',{autoClose:true});
 }
 
 function fault(res,fn,desc,param,warn=true) {
@@ -71,47 +59,47 @@ function succeed(res,fn,param,desc) {
     res.end(JSON.stringify(logger.log(desc,msg)));
 }
 
-async function  play(res,ipcData) {
-    logger.log('收到播放请求',{id:ipcData.id,fn:'live'});
-    let l=await getLive(ipcData);
+async function  play(res,id) {
+    logger.log('收到播放请求',{id:id,fn:'live'});
+    let l=await getLive(id);
     if(!l){
-        return fault(res,'live','请求的摄像头不存在',{id:ipcData.id});
+        return fault(res,'live','请求的摄像头不存在',{id});
     }
     if(l.running) {
-        return succeed(res,'live',{port:port,path:l.path,id:ipcData.id});
+        return succeed(res,'live',{port:port,path:l.path,id});
     }
     l.openWSS().then((ok)=>{
         if(!ok){
-            return fault(res,'live','获取直播流错误',{id:ipcData.id},false);
+            return fault(res,'live','获取直播流错误',{id},false);
         }
         l.on('close',()=>{
-            lives[ipcData.id]=null;
+            lives[id]=null;
             send({type:'countChanged',count:--lives.length});
         });
         send({type:'countChanged',count:++lives.length});
-        return succeed(res,'live',{port:port,path:l.path,id:ipcData.id});
+        return succeed(res,'live',{port:port,path:l.path,id});
     })
 }
 
-async function arrchive(res,ipcData,hid) {
-    logger.log('收到存档请求',{id:ipcData.id,hid,fn:'arrchive'});
-    let l=await getLive(ipcData);
-    if(!l) return fault(res,'arrchive','请求的摄像头不存在',{id:ipcData.id,hid});
+async function arrchive(res,id,hid) {
+    logger.log('收到存档请求',{id,hid,fn:'arrchive'});
+    let l=await getLive(id);
+    if(!l) return fault(res,'arrchive','请求的摄像头不存在',{id,hid});
     l.arrchive(hid).then((ok)=>{
-        return ok?succeed(res,'arrchive',{id:ipcData.id,hid}):fault(res,'arrchive','发生内部错误，无法存档视频流或无法打开视频流',{id:ipcData.id,hid},false);
+        return ok?succeed(res,'arrchive',{id,hid}):fault(res,'arrchive','发生内部错误，无法存档视频流或无法打开视频流',{id,hid},false);
     });
 }
 
-function stopArrchive(res,ipcData) {
-    logger.log('收到存档终止请求',{id:ipcData.id,fn:'stopArrchive'});
-    let l= lives[ipcData.id];
-    if(!l) return fault(res,'stopArrchive','请求的摄像头不存在',{id:ipcData.id});
-    l.stopArrchive(ipcData.id);
-    return succeed(res,'stopArrchive',{id:ipcData.id});
+function stopArrchive(res,id) {
+    logger.log('收到存档终止请求',{id,fn:'stopArrchive'});
+    let l= lives[id];
+    if(!l) return fault(res,'stopArrchive','请求的摄像头不存在',{id});
+    l.stopArrchive(id);
+    return succeed(res,'stopArrchive',{id});
 }
 
-async function ptz(res,ipcData,fun,params){
-    logger.log('收到ptz申请',{id:ipcData.id,fn:'ptz',op:fun});
+async function ptz(res,id,fun,params){
+    logger.log('收到ptz申请',{id,fn:'ptz',op:fun});
     params=params||{};
     const force=!!params.force;
     const stop=params.stop!=='0';
@@ -119,11 +107,11 @@ async function ptz(res,ipcData,fun,params){
     if(_.findIndex(ptzFun,(item)=>{return fun===item;})===-1) {
         return fault(res,'ptz','错误的PTZ命令',{op:fun});
     }
-    let ipc=await IPCFactory.getIPC(ipcData).catch(async ()=>{
+    let ipc=await IPCFactory.getIPC(id).catch(async ()=>{
         await Promise.resolve(null);
     });
     if(!ipc){
-        return fault(res,'ptz','请求的摄像头不存在',{id:ipcData.id});
+        return fault(res,'ptz','请求的摄像头不存在',{id});
     }
     ipc.ptz=ipc.ptz||{};
     const now=new Date().getTime();
@@ -131,15 +119,15 @@ async function ptz(res,ipcData,fun,params){
         ipc.ptz.handle=handle=createID();
     }
     else if(ipc.ptz.handle!==handle){
-        return fault(res,'ptz','ptz暂时由其他人控制中，请等待',{id:ipcData.id});
+        return fault(res,'ptz','ptz暂时由其他人控制中，请等待',{id});
     }
     ipc.ptz.lastTime=now;
     let promise=fun==='move'?ipc[fun].apply(ipc,[parseInt(params.position)||1,stop]):ipc[fun].apply(ipc,[stop]);
     promise.then(()=>{
-        succeed(res,'ptz',{handle,id:ipcData.id,op:fun,limit:ptzLock})
+        succeed(res,'ptz',{handle,id,op:fun,limit:ptzLock})
     }).catch(e=>{
         ipc.ptz.handle=null;
-        return fault(res,'ptz','内部处理异常',{id:ipcData.id,op:fun,innerError:e},false);
+        return fault(res,'ptz','内部处理异常',{id,op:fun,innerError:e},false);
     });
 }
 
@@ -149,22 +137,22 @@ releaseMic(handle,id){}
 sendMic(id,data){}
 */
 
-async function freePTZ(res,ipcData,params) {
+async function freePTZ(res,id,params) {
     params=params||{};
     let handle=params.handle;
-    logger.log('收到ptz释放申请',{id:ipcData.id,fn:'freePTZ',handle});
+    logger.log('收到ptz释放申请',{id,fn:'freePTZ',handle});
     if(!handle){
-        return fault(res,'freePTZ','不具备PTZ的控制权',{id:ipcData.id});
+        return fault(res,'freePTZ','不具备PTZ的控制权',{id});
     }
-    let ipc=await IPCFactory.getIPC(ipcData).catch(async ()=>{
+    let ipc=await IPCFactory.getIPC(id).catch(async ()=>{
         await Promise.resolve(null);
     });
-    if(!ipc)return fault(res,'freePTZ','请求的摄像头不存在',{id:ipcData.id});
+    if(!ipc)return fault(res,'freePTZ','请求的摄像头不存在',{id});
     if(handle===ipc.ptz.handle){
         ipc.ptz.handle='';
-        return succeed(res,'freePTZ',{id:ipcData.id,handle});
+        return succeed(res,'freePTZ',{id,handle});
     }
-    return fault(res,'freePTZ','不具备PTZ的控制权',{id:ipcData.id,handle});
+    return fault(res,'freePTZ','不具备PTZ的控制权',{id,handle});
 }
 
 const server=http.createServer();
@@ -176,34 +164,12 @@ server.on('request',(req,res)=>{
     let uri=url.parse(req.url,true);
     let paths=_.trim(uri.pathname,'/').split('/').concat(['','','','']);
     if(paths[0]!=='ipc') {
+        logger.warn('未知的请求',{uri});
         return;
     }
-    let str=url.parse(req.url).query;
-    //Logger.info("string",{str:str});
-    let ipcData = qs.parse(str);
-    delete ipcData.t;
-    let ipc = {
-        id:ipcData.id,
-        ip:ipcData.ip,
-        port:ipcData.port,
-        user:ipcData.user,
-        pwd:ipcData.pwd,
-        brand:ipcData.brand,
-        functions:{
-            ptz:ipcData.ptz,
-            alarm:ipcData.alarm,
-            audio:ipcData.audio
-        },
-        onvif:{
-            user:ipcData.onvif_user,
-            pwd:ipcData.onvif_pwd,
-            port:ipcData.onvif_port,
-            path:ipcData.onvif_path
-        }
-    };
-
-    res.setHeader('Connection','Upgrade');
-    res.setHeader('Upgrade','websocket');
+    logger.log('收到请求',{uri});
+    //res.setHeader('Connection','Upgrade');
+    //res.setHeader('Upgrade','websocket');
     res.setHeader('Content-Type','application/json; charset=utf-8');
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
@@ -217,31 +183,33 @@ server.on('request',(req,res)=>{
         return res.end();
     }
     if(index===0){
-        return play(res,ipc);
+        return play(res,id);
     }
     if(index===1){
-        return stopArrchive(res,ipc);
+        return stopArrchive(res,id);
     }
     if(index===2){
         //ptz/id/ptz/zoomAdd?position
-        return ptz(res,ipc,paths[3],uri.query);
+        return ptz(res,id,paths[3],uri.query);
     }
     if(index===3){
-        return freePTZ(res,ipc,uri.query);
+        return freePTZ(res,id,uri.query);
     }
     if(index===4){
-        return arrchive(res,ipc,paths[3]);
+        return arrchive(res,id,paths[3]);
     }
+
+    logger.warn('未知的方法请求',{fun,uri});
 });
 
-function listen() {
-    getPort().then(_port => {
-        server.listen(_port);
-        port=_port;
-    });
+async function listen() {
+    let _port=await getPort();
+    server.listen(_port);
+    port=_port;
 }
 
 server.on('listening',()=>{
+    connect();
     Parser(logger,'ipc_server_child.js',{port:server.address().port});
     logger.log('摄像头直播流进程启动',{processID:process.pid,port:port});
     send({type:'listening',port:port});
@@ -253,7 +221,8 @@ server.on('error',(err)=>{
     }
     console.error({source:'ipc_server_child.js',desc:err.toString()});
 });
-listen();
+
+listen().catch();
 
 function createID() {
     const hash = crypto.createHash('md5');
