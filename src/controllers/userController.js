78fe110d7cf084fb12e4a0f643ba82712070af6e
chenrui2014 <chenruixiang2009@163.com>
2018-04-09 23:54:49 +0800

    添加角色中文名称

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -49,7 +49,13 @@ class UserController {
     static async edit_user(ctx){
         const data = ctx.request.body;
         logger.info(data);
-        data.password = md5(data.password);
+        const user = await UserService.find_user({username:data.username,password:data.password});
+        console.log(data.password);
+        console.log(user);
+        console.log(user.toString());
+        if(user === null || user.length === 0){//如果密码被修改则需要加密，否则不需要
+            data.password = md5(data.password);
+        }
         let _id = data._id;
         delete data._id;
         const result = await UserService.edit_user({_id:_id},data);
